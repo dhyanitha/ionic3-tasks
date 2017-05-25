bfb7bfef5508194185168e8e88e11835703648ed
minibhati93 <minibhati93@gmail.com>
2017-05-25 23:15:51 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,18 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2';
+
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyDuJ9YArpEIscOX5_iPBeMhPldLiTNlmQw",
+    authDomain: "my-task-planner.firebaseapp.com",
+    databaseURL: "https://my-task-planner.firebaseio.com",
+    projectId: "my-task-planner",
+    storageBucket: "my-task-planner.appspot.com",
+    messagingSenderId: "1065892340674"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +30,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
